9f6b3ad39314d37fe4d29bbaec2ce0854cd3204e
Israel Oyedele <israeloyedele533@gmail.com>
2026-06-22 09:59:13 +0100

    [IO][sm] modified the footer --feat

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,7 +1,7 @@
 export default function Footer() {
   return (
     <footer>
-      <p className="text-center text-sm text-muted-foreground">
+      <p className="text-center text-xs md:text-sm text-muted-foreground">
         &copy; {new Date().getFullYear()} Israel Oyedele. All rights reserved.
       </p>
     </footer>
